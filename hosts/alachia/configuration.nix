b08cf689d3029ea145f1213c59eabc15f7d084a8
Anders Østhus <anders@osthus.me>
2025-12-07 12:09:59 +0100

    cachix for hyprland

--- hosts/alachia/configuration.nix
+++ hosts/alachia/configuration.nix
@@ -57,7 +57,12 @@
     nerd-fonts.jetbrains-mono
   ];
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
 
   programs.mtr.enable = true;
   programs.gnupg.agent = {
